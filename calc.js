db200e2656d3533adf1ce4e62ee2d11d325aba1d
wemsilva <weliton8424@gmail.com>
2021-05-19 18:23:21 -0300

    Commit dos arquivos

---
+++ calc.js
@@ -0,0 +1,26 @@
+const calc = document.getElementById('calc');
+
+
+
+function gasoGasto () {
+    
+    const kmviagem = document.getElementById('kmviagem').value;
+    const gasolina = document.getElementById('gasolina').value;
+    const kmlitro = document.getElementById('kmlitro').value;
+    const resultado = document.getElementById('resultado');
+
+    if (kmviagem !== '' && gasolina !== '' && kmlitro !== '') {
+
+        const gasto = ((kmviagem * gasolina.replace(',' , '.')) / kmlitro);
+
+
+        resultado.textContent = `Gasto é ${gasto.toLocaleString('pt-BR', {style: 'currency', currency:'BRL' })}`
+    }
+    else {
+
+        window.alert('Por favor preencha todos os campos!')
+    }
+
+};
+
+calc.addEventListener('click', gasoGasto);
